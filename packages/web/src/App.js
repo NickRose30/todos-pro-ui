7cbb9fea121f0d97c1c9a1fd9e6c19a36d4eef62
NickRose30 <nicholasrose30@gmail.com>
2020-07-02 00:52:26 -0400

    remove reference to nonexistent code

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { double } from '@monorepo/common';
 import styled from 'styled-components';
 
 const H1 = styled.h1`
   color: blue;
 `;
 
-export default _ => (
-  <H1>Double 2 is {double(2)}</H1>
-);
+export default _ => <H1>Welcome to the web app</H1>;
